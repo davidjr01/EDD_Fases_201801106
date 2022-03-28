f83d6a205141b20251b1bbc7ef85588fc9c0e165
davidjr01 <davidljr01@gmail.com>
2022-03-28 01:18:20 -0600

    arbol avl

--- EDD_Fase2/src/edd_fase2/AVL.java
+++ EDD_Fase2/src/edd_fase2/AVL.java
@@ -116,6 +116,162 @@ public class AVL {
     
     // ---------------------------------------------- Funciones para buscar ----------------------------------------------------------
     
+    public Nodo search(int id){
+        if (this.root==null){
+            return null;
+        }
+        else{
+            return this._search(id, this.root);
+        }
+    }
+
+    public Nodo _search(int id, Nodo node){
+        if (node ==null){
+            return null;
+        }
+        else if(id < node.id){
+            return this._search( id, node.izquierda );
+        }
+        else if ( id > node.id){
+            return this._search( id, node.derecha );
+        }
+        else{
+            return node;
+        }
+    }
+
+    public Nodo getMin(){
+            if (this.root ==null){
+                return null;
+            }
+            else{
+                return this._getMin( this.root );
+            }
+    }
+
+    public Nodo _getMin(Nodo node){
+        if (node.izquierda){
+            return this._getMin( node.izquierda );
+        }
+        else{
+            return node;
+        }
+    }
+    
+    public Nodo getMax(){
+        if (this.root==null){
+            return null;
+        }
+        else{
+            return this._getMax( this.root );
+        }
+    }
+
+    public Nodo _getMax(Nodo node){
+        if (node.derecha){
+            return this._getMax( node.derecha );
+        }
+        else{
+            return node;
+        }
+    }
+
+    //------------------------------------------------ Eliminar nodo del arbol ------------------------------------------------------
+    public void delete(int id){
+        this.root = this._delete(id, this.root);
+    }
+
+    public Nodo _delete(int id, Nodo node){
+        if (node ==null){
+            System.out.println("imagen no registrada");
+        }
+
+        else if (id < node.id){
+            node.izquierda = this._delete(id, node.izquierda);
+            if ((this.height(node.derecha) - this.height(node.izquierda)) == 2){
+                if (this.height(node.derecha.derecha) >= this.height(node.derecha.izquierda)){
+                    node = this.SimpleDer(node);
+                }
+                else{
+                    node = this.dobleDer(node);
+                }
+            }
+            node.tamaño= this.max_min(this.height(node.izquierda), this.height(node.derecha)) + 1;
+        }
+            
+        else if(id>node.id) {
+            node.derecha = this._delete(id, node.derecha);
+            if ((this.height(node.izquierda) - this.height(node.derecha)) == 2){
+                if (this.height(node.izquierda.izquierda) >= this.height(node.izquierda.derecha)){
+                    node = this.SimpleIzq(node);
+                }
+                else{
+                    node = this.dobleIzq(node);
+                }
+            }
+            node.tamaño = this.max_min(this.height(node.izquierda), this.height(node.derecha))  +1;
+        }
+        
+        else if((node.izquierda) && (node.derecha)) {
+            if (node.izquierda.tamaño <= node.derecha.tamaño){
+                Nodo minNode = this._getMin(node.derecha);
+                node.id = minNode.id;
+                node.derecha = this._delete(node.id, node.derecha);
+            }
+            else{
+                Nodo maxNode = this._getMax(node.izquierda);
+                node.id = maxNode.id;
+                node.izquierda = this._delete(node.id,node.izquierda);
+            }
+            node.tamaño = this.max_min(this.height(node.izquierda), this.height(node.derecha)) + 1;
+        }
+        else{
+            if (node.derecha){
+                node = node.derecha;
+            }
+            else{
+                node = node.izquierda;
+            }
+        }
+        
+        return node;
+    }
+
+    // ------------------------------------------------- Recorrcarnetos del arbol --------------------------------------------------------
+    public void preorden(){
+        this._preorden(this.root);
+    }
+
+    public void _preorden(Nodo root){
+        if (root !=null){
+            System.out.println(root.id);
+            this._preorden(root.izquierda);
+            this._preorden(root.derecha);
+        }
+    }
+    
+    public void inorden(){
+        this._inorden(this.root);
+     }
+    public void _inorden(Nodo root){
+        if (root !=null){
+            this._inorden(root.izquierda);
+            System.out.println(root.id);
+            this._inorden(root.derecha);
+        }
+    } 
+
+    public void postorden(){
+        this._postorden(this.root);
+    }
+
+    public void _postorden(Nodo root){
+        if (root !=null){
+            this._postorden(root.izquierda);
+            this._postorden(root.derecha);
+            System.out.println(root.id);
+        }
+    }
     
     
     
